8821bb61de449fd25bb983ebf6c73c8c6bd70de1
kozistr <kozistr@gmail.com>
2023-07-09 14:31:36 +0900

    update: server

--- src/server.rs
+++ src/server.rs
@@ -45,40 +45,43 @@ pub struct EmbeddedBook {
     pub embeddings: [f32; 384],
 }
 
-// impl EmbeddedBook {
-//     fn topic(embeddings: [f32; 384]) -> Self {
-//         Self {
-//             title: None,
-//             author: None,
-//             summary: None,
-//             embeddings,
-//         }
-//     }
-// }
-
 fn to_array(barry: &[f32]) -> [f32; 384] {
     barry.try_into().expect("slice with incorrect length")
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let now: Instant = Instant::now();
     let model: SentenceEmbeddingsModel =
         SentenceEmbeddingsBuilder::remote(AllMiniLmL12V2).create_model()?;
+    println!("load model : {:?}", now.elapsed());
 
     let now: Instant = Instant::now();
     let json: String = fs::read_to_string("data/books.json")?;
     let library: Library = serde_json::from_str(&json)?;
     println!("load data : {:?}", now.elapsed());
 
-    let mut sentences: Vec<String> = Vec::new();
-    for book in library.books.clone() {
-        sentences.push(book.summary);
-    }
+    // let mut summaries: Vec<String> = Vec::new();
+    // for book in library.books.clone() {
+    //     summaries.push(book.summary);
+    // }
+
+    // let now: Instant = Instant::now();
+    // let embeddings: Vec<Vec<f32>> = model.encode(&summaries)?;
+    // println!(
+    //     "batch inference ({:?} documents) : {:?}",
+    //     summaries.len(),
+    //     now.elapsed()
+    // );
 
     let now: Instant = Instant::now();
-    let embeddings: Vec<Vec<f32>> = model.encode(&sentences)?;
+    let mut embeddings: Vec<[f32; 384]> = Vec::new();
+    for book in library.books.iter() {
+        let embedding: Vec<Vec<f32>> = model.encode(&[book.clone().summary])?;
+        embeddings.push(to_array(&embedding[0]));
+    }
     println!(
-        "batch inference ({:?} documents) : {:?}",
-        sentences.len(),
+        "inference ({:?} documents) : {:?}",
+        embeddings.len(),
         now.elapsed()
     );
 
@@ -90,21 +93,19 @@ fn main() -> Result<(), Box<dyn Error>> {
     let now: Instant = Instant::now();
     let mut index: HNSWIndex<f32, usize> =
         HNSWIndex::<f32, usize>::new(384, &HNSWParams::<f32>::default());
-    for (i, sample) in embeddings.iter().enumerate() {
-        index.add(sample, i).unwrap();
+    for (i, embedding) in embeddings.iter().enumerate() {
+        index.add(embedding, i).unwrap();
     }
     index.build(Euclidean).unwrap();
-    println!("set index : {:?}", now.elapsed());
+    println!("build index : {:?}", now.elapsed());
 
     let query: &str = "The story about prep school";
-    println!("Querying: {}", query);
+    println!("query : {}", query);
 
     let query_embeddings: Vec<Vec<f32>> = model.encode(&[query])?;
-    // let query_embedding = to_array(query_embeddings[0].as_slice());
-    // let query_topic = EmbeddedBook::topic(query_embedding);
 
     let now: Instant = Instant::now();
-    let neighbor_index: Vec<usize> = index.search(&query_embeddings[0], 5);
+    let neighbor_index: Vec<usize> = index.search(&query_embeddings[0], 10);
     println!("search speed : {:?}", now.elapsed());
 
     for (k, idx) in neighbor_index.iter().enumerate() {
