040b6879f437baf05cd8ac1e71907b8dd8be8a26
kozistr <kozistr@gmail.com>
2023-07-11 00:18:15 +0900

    update: warmup

--- src/client.rs
+++ src/client.rs
@@ -11,7 +11,8 @@ static GLOBAL: MiMalloc = MiMalloc;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let mut client = ss::inference_client::InferenceClient::connect("http://127.0.0.1:50051").await?;
+    let mut client =
+        ss::inference_client::InferenceClient::connect("http://127.0.0.1:50051").await?;
 
     let request = ss::PredictRequest {
         features: vec![
@@ -23,8 +24,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
         ],
     };
 
-    let result = client.predict(request.clone()).await?;
-    println!("result : {:?}", result);
+    for _ in 0..10 {
+        _ = client.predict(request.clone()).await?;
+    }
+
+    let response = client.predict(request).await?.into_inner();
+    println!("response : {:?}", response.indices);
 
     Ok(())
 }
