8454f1984311b4076f29e8b55551084da0a90996
kozistr <kozistr@gmail.com>
2023-07-10 23:08:47 +0900

    update: client

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,32 @@
+use std::error::Error;
+
+pub mod ss {
+    tonic::include_proto!("ss");
+}
+
+#[global_allocator]
+static GLOBAL: MiMalloc = MiMalloc;
+
 fn main() {
-    println!("Hello, world!");
+    let mut client = ss::inference_client::VectorSearchService::connect("127.0.0.1:50051").await?;
+
+    let request = ss::PredictRequest {
+        features: vec![
+            ss::Features {
+                query: "The story about the school life",
+                k: 10,
+            };
+            8
+        ],
+    };
+
+    // warm up 10 times
+    for _ in 0..10 {
+        _ = client.search(request.clone()).await?;
+    }
+
+    let result = client.search(request.clone()).await?;
+    println!("result : {:?}", result);
+
+    Ok(())
 }
