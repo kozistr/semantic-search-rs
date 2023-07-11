e4536a652036ccf690f67f0df3a798cb385abe6f
kozistr <kozistr@gmail.com>
2023-07-11 14:18:34 +0900

    update: log latency

--- src/search.rs
+++ src/search.rs
@@ -28,27 +28,45 @@ fn load_index() -> HNSWIndex<f32, usize> {
     HNSWIndex::<f32, usize>::load("index.hora").unwrap()
 }
 
+#[allow(dead_code)]
+pub fn preprocess(features: &Vec<Features>) -> (Vec<Vec<String>>, Vec<Vec<usize>>) {
+    let query: Vec<Vec<String>> = features
+        .iter()
+        .map(|f: &Features| vec![f.query.clone()])
+        .collect();
+
+    let k: Vec<Vec<usize>> = features
+        .iter()
+        .map(|f: &Features| vec![f.k as usize])
+        .collect();
+
+    (query, k)
+}
+
 pub fn search(request: PredictRequest) -> PredictResponse {
+    // let (query, k) = preprocess(&request.features);
     let feature: &Features = &request.features.first().clone().unwrap();
     let query = feature.query.clone();
     let k: usize = feature.k.clone() as usize;
 
-    let now: Instant = Instant::now();
+    let start: Instant = Instant::now();
     let query_embeddings: Vec<Vec<f32>> =
-        MODEL.with(|model: &SentenceEmbeddingsModel| model.encode(&[query]).unwrap());
-    println!("embedding : {:?}", now.elapsed());
+        MODEL.with(|model: &SentenceEmbeddingsModel| model.encode(&query).unwrap());
+    let model_latency: u64 = start.elapsed().as_nanos() as u64;
 
-    let now: Instant = Instant::now();
+    let start: Instant = Instant::now();
     let neighbor_index: Vec<usize> =
         INDEX.with(|index: &HNSWIndex<f32, usize>| index.search(&query_embeddings[0], k));
-    println!("search : {:?}", now.elapsed());
+    let search_latency: u64 = start.elapsed().as_nanos() as u64;
 
     PredictResponse {
         indices: neighbor_index
             .iter()
-            .map(|index| Index {
+            .map(|index: &usize| Index {
                 index: *index as i32,
             })
             .collect(),
+        model_latency,
+        search_latency,
     }
 }
