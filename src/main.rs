7b3bc3805b7ff4a5c6759ef1515e9a94c7a80e96
kozistr <kozistr@gmail.com>
2023-07-13 21:38:30 +0900

    update: main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::{
+    fs::{File, OpenOptions},
+    io::BufReader,
+    path::PathBuf,
+};
+
+use semantic_search::hnsw_index::{
+    dist::DistL2,
+    hnsw::Hnsw,
+    hnswio::{load_description, load_hnsw, Description},
+};
+
+fn load_file(filename: &str) -> BufReader<File> {
+    let path: PathBuf = PathBuf::from(filename.to_string());
+    let res: File = OpenOptions::new().read(true).open(&path).unwrap();
+    let reader: BufReader<File> = BufReader::new(res);
+    reader
+}
+
+fn load_index() -> Hnsw<f32, DistL2> {
+    println!("load index");
+    let mut graph: BufReader<File> = load_file("index.hnsw.graph");
+    let mut data: BufReader<File> = load_file("index.hnsw.data");
+
+    let description: Description = load_description(&mut graph).unwrap();
+    let index: Hnsw<f32, DistL2> = load_hnsw(&mut graph, &description, &mut data).unwrap();
+    index
+}
+
 fn main() {
-    println!("Hello, world!");
+    let index: Hnsw<f32, DistL2> = load_index();
 }
