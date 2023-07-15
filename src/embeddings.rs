7d0f1119cfe26cf014527ed2fa850bf804f95879
kozistr <kozistr@gmail.com>
2023-07-15 17:16:10 +0900

    refactor: embeddings

--- src/embeddings.rs
+++ src/embeddings.rs
@@ -1,24 +1,39 @@
 use anyhow::Result;
-// use hora::{
-//     core::{ann_index::ANNIndex, ann_index::SerializableIndex, metrics::Metric::Euclidean},
-//     index::{hnsw_idx::HNSWIndex, hnsw_params::HNSWParams},
-// };
 use csv::Reader;
 use indicatif::ProgressBar;
 use mimalloc::MiMalloc;
-use rust_bert::pipelines::sentence_embeddings::{
-    SentenceEmbeddingsBuilder, SentenceEmbeddingsModel, SentenceEmbeddingsModelType::AllMiniLmL12V2,
-};
+use rust_bert::pipelines::sentence_embeddings::SentenceEmbeddingsModel;
 use serde::Deserialize;
 use std::{
+    env,
     fs::{read_to_string, File},
+    process,
     time::Instant,
 };
+
+mod utils;
+use utils::load_model;
 
 use semantic_search::hnsw_index::{api::AnnT, dist::DistL2, hnsw::Hnsw};
 
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
+
+#[derive(Debug, Clone)]
+struct Config {
+    dataset: String,
+}
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 1 {
+            return Err("not enough arguments");
+        }
+
+        let dataset: String = args[0].to_string();
+
+        Ok(Config { dataset })
+    }
+}
 
 #[derive(Debug, Deserialize)]
 pub struct Library {
@@ -35,44 +50,42 @@ pub struct Book {
 }
 
 fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    let config: Config = Config::new(&args).unwrap_or_else(|err: &str| {
+        println!("Problem parsing arguments: {}", err);
+        println!("Usage: embeddings dataset (news or book)");
+        process::exit(1);
+    });
+
     let start: Instant = Instant::now();
-    // let model: SentenceEmbeddingsModel =
-    //     SentenceEmbeddingsBuilder::remote(AllMiniLmL12V2).create_model()?;
-    let model: SentenceEmbeddingsModel =
-        SentenceEmbeddingsBuilder::local("models").create_model()?;
+    let model: SentenceEmbeddingsModel = load_model();
     println!("load model : {:.3?}", start.elapsed());
 
     let start: Instant = Instant::now();
-    // let json: String = read_to_string("data/books.json")?;
-    // let library: Library = serde_json::from_str(&json)?;
-    // let summaries: Vec<String> = library
-    //     .books
-    //     .iter()
-    //     .map(|book: &Book| book.summary.clone())
-    //     .collect();
-    let file: File = File::open("data/ag_news.csv")?;
-    let mut reader = Reader::from_reader(file);
-    let summaries: Vec<String> = reader
-        .records()
-        .map(|res| res.unwrap()[0].to_string())
-        .collect();
+
+    let data: Vec<String>;
+    if config.dataset == "book" {
+        let json: String = read_to_string("data/books.json")?;
+        let library: Library = serde_json::from_str(&json)?;
+
+        data = library
+            .books
+            .iter()
+            .map(|book: &Book| book.summary.clone())
+            .collect();
+    } else {
+        let file: File = File::open("data/ag_news.csv")?;
+        let mut reader = Reader::from_reader(file);
+
+        data = reader
+            .records()
+            .map(|res| res.unwrap()[0].to_string())
+            .collect();
+    }
     println!("load data : {:.3?}", start.elapsed());
 
-    // hora
-    // let mut index: HNSWIndex<f32, usize> =
-    //     HNSWIndex::<f32, usize>::new(384, &HNSWParams::<f32>::default());
-
-    // library.books.iter().enumerate().for_each(|(idx, book)| {
-    //     if let Ok(embedding) = model.encode(&[book.summary.clone()]) {
-    //         index.add(&embedding[0], idx).unwrap();
-    //     }
-    // });
-
-    // index.build(Euclidean)?;
-    // index.dump("index.hora")?;
-
-    // hnswlib
-    let nb_elem: usize = summaries.len();
+    let nb_elem: usize = data.len();
     let max_nb_connection: usize = 16;
     let ef_c: usize = 200;
     let nb_layer: usize = 16;
@@ -83,7 +96,7 @@ fn main() -> Result<()> {
 
     let bs: usize = 128;
     let pb = ProgressBar::new((nb_elem / bs + 1) as u64);
-    for chunk in summaries.chunks(bs) {
+    for chunk in data.chunks(bs) {
         let embeds: Vec<Vec<f32>> = model.encode(chunk).unwrap();
         embeddings.extend(embeds.into_iter());
         pb.inc(1);
@@ -99,7 +112,7 @@ fn main() -> Result<()> {
     index.parallel_insert(&embeddings_indices);
     println!("parallel insert : {:.3?}", start.elapsed());
 
-    _ = index.file_dump(&"news".to_string());
+    _ = index.file_dump(&config.dataset);
 
     Ok(())
 }
