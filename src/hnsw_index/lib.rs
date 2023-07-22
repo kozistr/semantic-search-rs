83075db90f9fa1fa5401ccdf5cccd1196bc2194e
kozistr <kozistr@gmail.com>
2023-07-22 17:10:52 +0900

    update: remove comments

--- src/hnsw_index/lib.rs
+++ src/hnsw_index/lib.rs
@@ -1,9 +1,3 @@
-//#![feature(portable_simd)]
-// prededing line to uncomment to get std::simd by using
-// packed_simd_2 = { version = "0.3", optional = true}
-// and compile with cargo [test|build] --features "stdsimd" ...
-
-// for logging (debug mostly, switched at compile time in cargo.toml)
 use env_logger::Builder;
 
 #[macro_use]
