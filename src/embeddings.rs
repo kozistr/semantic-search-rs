0b2e004a55e87e6aff44abbd902d70b3b5879401
kozistr <kozistr@gmail.com>
2023-07-22 17:40:16 +0900

    feature: support quantization

--- src/embeddings.rs
+++ src/embeddings.rs
@@ -3,13 +3,19 @@ use std::time::Instant;
 use anyhow::Result;
 #[cfg(feature = "progress")]
 use indicatif::ProgressBar;
+use rayon::prelude::*;
 use rust_bert::pipelines::sentence_embeddings::SentenceEmbeddingsModel;
 use semantic_search::hnsw_index::api::AnnT;
 use semantic_search::hnsw_index::dist::DistDot;
-use semantic_search::hnsw_index::hnsw::Hnsw;
+use semantic_search::hnsw_index::hnsw::{quantize, Hnsw};
 use semantic_search::utils::{load_data, load_model};
 
 fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    let do_quantize: bool = if args.len() < 2 { false } else { true };
+    println!("do quantize (f32 to i8) : {:?}", do_quantize);
+
     let start: Instant = Instant::now();
     let model: SentenceEmbeddingsModel = load_model();
     println!("load model : {:.3?}", start.elapsed());
@@ -22,8 +28,12 @@ fn main() -> Result<()> {
     let max_nb_connection: usize = 16;
     let ef_c: usize = 200;
     let nb_layer: usize = 16;
-    let index: Hnsw<f32, DistDot> =
-        Hnsw::<f32, DistDot>::new(max_nb_connection, nb_elem, nb_layer, ef_c, DistDot {});
+
+    let index: Hnsw<_, DistDot> = if !do_quantize {
+        Hnsw::<f32, DistDot>::new(max_nb_connection, nb_elem, nb_layer, ef_c, DistDot {})
+    } else {
+        Hnsw::<i8, DistDot>::new(max_nb_connection, nb_elem, nb_layer, ef_c, DistDot {})
+    };
 
     let mut embeddings: Vec<Vec<f32>> = Vec::with_capacity(nb_elem);
 
@@ -54,14 +64,26 @@ fn main() -> Result<()> {
 
     println!("inference : {:.3?}", pb.elapsed());
 
-    let embeddings_indices: Vec<(&Vec<f32>, usize)> =
+    let embeddings: Vec<Vec<_>> = if do_quantize {
+        let start: Instant = Instant::now();
+        let embeddings: Vec<Vec<i8>> = embeddings
+            .par_iter()
+            .map(|embedding: &Vec<f32>| quantize(embedding))
+            .collect();
+        println!("quantize : {:.3?}", start.elapsed());
+        embeddings
+    } else {
+        embeddings
+    };
+
+    let embeddings_indices: Vec<(&Vec<i8>, usize)> =
         embeddings.iter().zip(0..embeddings.len()).collect();
 
     let start: Instant = Instant::now();
     index.parallel_insert(&embeddings_indices);
     println!("parallel insert : {:.3?}", start.elapsed());
 
-    _ = index.file_dump(&"news".to_string());
+    _ = index.file_dump(&"news_q".to_string());
 
     Ok(())
 }
