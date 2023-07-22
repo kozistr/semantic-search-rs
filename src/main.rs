c02ab37318f2e95e27229c8a9d4f19c9449bcdfe
kozistr <kozistr@gmail.com>
2023-07-22 18:20:50 +0900

    update: example

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::env;
-use std::time::Instant;
 
 use rust_bert::pipelines::sentence_embeddings::SentenceEmbeddingsModel;
 use semantic_search::hnsw_index::dist::DistDot;
-use semantic_search::hnsw_index::hnsw::{Hnsw, Neighbour};
-use semantic_search::utils::{load_data, load_index, load_model};
+#[allow(unused_imports)]
+use semantic_search::hnsw_index::hnsw::{quantize, Hnsw, Neighbour};
+#[allow(unused_imports)]
+use semantic_search::utils::{load_data, load_index, load_model, load_quantize_index};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -17,12 +18,16 @@ fn main() {
     println!("query : {:?}", query);
 
     let model: SentenceEmbeddingsModel = load_model();
-    let index: Hnsw<f32, DistDot> = load_index("news");
+    // let index: Hnsw<f32, DistDot> = load_index("news");
+    let index: Hnsw<i8, DistDot> = load_quantize_index("news");
     let data: Vec<String> = load_data();
 
     let query_embedding: Vec<Vec<f32>> = model.encode(&[query]).unwrap();
 
-    let neighbors: Vec<Neighbour> = index.search(&query_embedding[0], 10, 30);
+    let query_embedding: Vec<i8> = quantize(&query_embedding[0]);
+
+    // let neighbors: Vec<Neighbour> = index.search(&query_embedding[0], 10, 30);
+    let neighbors: Vec<Neighbour> = index.search(query_embedding.as_slice(), 10, 30);
 
     for (k, neighbor) in neighbors.iter().enumerate() {
         println!("top {} | id : {}, dist : {}", k + 1, neighbor.d_id, neighbor.distance);
