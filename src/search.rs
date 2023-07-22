2e504128170877455c0fe1375cf98bea66c02d28
kozistr <kozistr@gmail.com>
2023-07-22 17:59:55 +0900

    update: search

--- src/search.rs
+++ src/search.rs
@@ -1,21 +1,24 @@
 use std::time::Instant;
 
 use mimalloc::MiMalloc;
+#[allow(unused_imports)]
 use rayon::prelude::*;
 use rust_bert::pipelines::sentence_embeddings::SentenceEmbeddingsModel;
 
 use crate::hnsw_index::dist::DistDot;
+#[allow(unused_imports)]
 use crate::hnsw_index::hnsw::{quantize, Hnsw, Neighbour};
 use crate::ss::{Features, Index, PredictRequest, PredictResponse};
-use crate::utils::{load_model, load_quantize_index};
+#[allow(unused_imports)]
+use crate::utils::{load_index, load_model, load_quantize_index};
 
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
 
 thread_local! {
     pub static MODEL: SentenceEmbeddingsModel = load_model();
-    // pub static INDEX: Hnsw<f32, DistDot> = load_index("news");
-    pub static INDEX: Hnsw<i8, DistDot> = load_quantize_index("news");
+    pub static INDEX: Hnsw<f32, DistDot> = load_index("news");
+    // pub static INDEX: Hnsw<i8, DistDot> = load_quantize_index("news");
 }
 
 pub fn preprocess(request: &PredictRequest) -> (Vec<String>, usize) {
@@ -38,14 +41,16 @@ pub fn search(request: PredictRequest) -> PredictResponse {
         MODEL.with(|model: &SentenceEmbeddingsModel| model.encode(&query).unwrap());
     let model_latency: u64 = start.elapsed().as_nanos() as u64;
 
-    let query_embeddings: Vec<Vec<i8>> = query_embeddings
-        .par_iter()
-        .map(|embedding: &Vec<f32>| quantize(embedding))
-        .collect();
+    // let query_embeddings: Vec<Vec<i8>> = query_embeddings
+    //     .par_iter()
+    //     .map(|embedding: &Vec<f32>| quantize(embedding))
+    //     .collect();
 
     let start: Instant = Instant::now();
     let neighbor_index: Vec<Vec<Neighbour>> =
-        INDEX.with(|index: &Hnsw<i8, DistDot>| index.parallel_search(&query_embeddings, k, 30));
+        INDEX.with(|index: &Hnsw<f32, DistDot>| index.parallel_search(&query_embeddings, k, 30));
+    // let neighbor_index: Vec<Vec<Neighbour>> =
+    //     INDEX.with(|index: &Hnsw<i8, DistDot>| index.parallel_search(&query_embeddings, k, 30));
     let search_latency: u64 = start.elapsed().as_nanos() as u64;
 
     PredictResponse {
