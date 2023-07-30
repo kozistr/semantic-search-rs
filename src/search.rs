c88db57ead8bf1547d1ddfd12f862b2982a88603
kozistr <kozistr@gmail.com>
2023-07-30 15:21:14 +0900

    update: allow unused imports

--- src/search.rs
+++ src/search.rs
@@ -5,7 +5,8 @@ use mimalloc::MiMalloc;
 use rayon::prelude::*;
 use rust_bert::pipelines::sentence_embeddings::SentenceEmbeddingsModel;
 
-use crate::hnsw_index::dist::DistDot;
+#[allow(unused_imports)]
+use crate::hnsw_index::dist::{DistDot, DistHamming};
 #[allow(unused_imports)]
 use crate::hnsw_index::hnsw::{quantize, Hnsw, Neighbour};
 use crate::ss::{Features, Index, PredictRequest, PredictResponse};
@@ -18,7 +19,7 @@ static GLOBAL: MiMalloc = MiMalloc;
 thread_local! {
     pub static MODEL: SentenceEmbeddingsModel = load_model();
     // pub static INDEX: Hnsw<f32, DistDot> = load_index("news");
-    pub static INDEX: Hnsw<i8, DistDot> = load_quantize_index("news");
+    pub static INDEX: Hnsw<i8, DistHamming> = load_quantize_index("news");
 }
 
 pub fn preprocess(request: &PredictRequest) -> (Vec<String>, usize) {
@@ -47,7 +48,7 @@ pub fn search(request: PredictRequest) -> PredictResponse {
     // let neighbor_index: Vec<Vec<Neighbour>> =
     //     INDEX.with(|index: &Hnsw<f32, DistDot>| index.parallel_search(&query_embeddings, k, 30));
     let neighbor_index: Vec<Vec<Neighbour>> =
-        INDEX.with(|index: &Hnsw<i8, DistDot>| index.parallel_search(&query_embeddings, k, 30));
+        INDEX.with(|index: &Hnsw<i8, DistHamming>| index.parallel_search(&query_embeddings, k, 30));
     let search_latency: u64 = start.elapsed().as_nanos() as u64;
 
     PredictResponse {
